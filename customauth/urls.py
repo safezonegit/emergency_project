6acd87b651b3f6e12c54b60610331b636a581b44
DestinyAmechi <destinyamechi6@gmail.com>
2025-02-13 12:56:29 +0000

    templates-change

--- customauth/urls.py
+++ customauth/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('register/',views.register,name='user_register'),
+    path('register/',views.register,name='register'),
     path('login/',views.login_view, name='login'),
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
 
