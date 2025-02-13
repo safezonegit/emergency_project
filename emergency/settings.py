b198619bb12243de4440b1f4a7f39083abad288e
DestinyAmechi <destinyamechi6@gmail.com>
2025-02-13 12:34:50 +0000

    templates-change

--- emergency/settings.py
+++ emergency/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,8 +51,7 @@ INSTALLED_APPS = [
     'incident.apps.IncidentConfig',
     # 3rd Party Apps
     'decouple',
-    'celery',
-
+    'celery', 
 
 ]
 
@@ -89,13 +89,12 @@ WSGI_APPLICATION = 'emergency.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
+
 
 
 # Password validation
